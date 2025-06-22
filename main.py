783e75e1f712e91b585c82fe1b867469757c3cdd
Patryk Baranowski <patrykbaranowski1996@gmail.com>
2025-06-22 23:13:18 +0200

    - created main.py, libraries.py, administation_panel.py

---
+++ main.py
@@ -0,0 +1,18 @@
+from libraries import tk
+from administration_panel import view_administration_panel
+from dotenv import load_dotenv
+load_dotenv() #loading API_key from .env
+
+##PANEL GŁÓWNY APP
+root = tk.Tk()
+root.title("Gastronomia - Panel Główny")
+root.geometry("500x500")
+
+menu_edit_button = tk.Button(root, text="Zarządzaj MENU", command=view_administration_panel)
+menu_edit_button.pack()
+
+# add_order_button = tk.Button(root, text="Dodaj zamówienie", command=open_orders)
+# add_order_button.pack()
+
+
+root.mainloop()
